60efe15d347ce6878796899343179ff7b055d07c
tsmluky <tsmluky@gmail.com>
2026-07-18 18:19:12 +0200

    Deploy clean workspace reset

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'studio32-hub-v2'
+const CACHE_NAME = 'studio32-hub-v3'
 const APP_SHELL = ['/', '/manifest.webmanifest', '/studio32-mark.svg']
 
 self.addEventListener('install', (event) => {
